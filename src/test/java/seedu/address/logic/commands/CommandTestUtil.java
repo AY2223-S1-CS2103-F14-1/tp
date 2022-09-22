29017caf82218443ea62c88b8c5d86f6239ef1ac
Linus <53803800+zupey@users.noreply.github.com>
2022-09-22 13:31:14 +0800

    Add Remark and Tests.

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -34,6 +34,8 @@ public class CommandTestUtil {
     public static final String VALID_EMAIL_BOB = "bob@example.com";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
+    public static final String VALID_REMARK_AMY = "ABCDEFG I love Amy Bee";
+    public static final String VALID_REMARK_BOB = "i dont like bob";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
